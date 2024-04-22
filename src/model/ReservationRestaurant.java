bf59d02590bab7996f54090f2beba3f649f23f3b
victorgh31 <victor.bertrand31@gmail.com>
2024-04-22 19:12:10 +0200

    ILU2-POO-TP5 : Création de la classe ReservationRestaurant.

--- src/model/ReservationRestaurant.java
+++ src/model/ReservationRestaurant.java
@@ -1,5 +1,18 @@
 package model;
 
-public class ReservationRestaurant {
-
+public class ReservationRestaurant extends Reservation {
+	private int numService;
+	private int numTableReservee;
+	
+	public ReservationRestaurant(int jour, int mois, int numService , int numTableReservee) {
+		super(jour, mois);
+		this.numService = numService;
+		this.numTableReservee = numTableReservee;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "table n°"+ numTableReservee + 
+				" pour le " + (numService == 1 ? "premier" : "deuxième") + " service.\n";
+	}
 }
