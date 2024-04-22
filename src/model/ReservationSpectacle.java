bc0e1061bf14ef80021162c28c682e4de56e4be4
victorgh31 <victor.bertrand31@gmail.com>
2024-04-22 19:13:48 +0200

    ILU2-POO-TP5 : Création de la classe ReservationSpectacle.

--- src/model/ReservationSpectacle.java
+++ src/model/ReservationSpectacle.java
@@ -1,5 +1,12 @@
 package model;
 
-public class ReservationSpectacle {
-
+public class ReservationSpectacle extends Reservation {
+	private int numZone;
+	private int numChaise;
+	
+	public ReservationSpectacle(int jour, int mois, int numZone ,int numChaise) {
+		super(jour, mois);
+		this.numChaise = numChaise;
+		this.numZone = numZone;
+	}
 }
