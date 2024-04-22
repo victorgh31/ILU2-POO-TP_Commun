456c8d28e3d2f08ff3aa76b6810b6a61848a316a
victorgh31 <victor.bertrand31@gmail.com>
2024-04-22 20:25:48 +0200

    ILU2-POO-TP5 : Le tests appliquées à la classe FormulaireRestaurant passent.

--- src/model/FormulaireRestaurant.java
+++ src/model/FormulaireRestaurant.java
@@ -4,7 +4,7 @@ public class FormulaireRestaurant extends Formulaire {
 	private int nombrePersonnes;
 	private int numService;
 	
-	public FormulaireRestaurant(int jour, int mois) {
+	public FormulaireRestaurant(int jour, int mois, int nombrePersonnes, int numService) {
 		super(jour, mois);
 		this.nombrePersonnes = nombrePersonnes;
 		this.numService = numService;
